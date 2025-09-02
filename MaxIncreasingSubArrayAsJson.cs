69f48f19e761d2060bfe9758ffbc5e875bbed679
Kadir AYDOĞDU <121028208+aydogdu25@users.noreply.github.com>
2025-09-02 19:06:48 +0300

    Update MaxIncreasingSubArrayAsJson.cs

--- MaxIncreasingSubArrayAsJson.cs
+++ MaxIncreasingSubArrayAsJson.cs
@@ -1,46 +1,41 @@
-﻿using System.Collections.Generic;
-using System.Text.Json;
+using System.Linq; 
+using System.Text.Json; 
+using System.Collections.Generic; 
 
-namespace ConsoleApp2
+public static string MaxIncreasingSubarrayAsJson(List<int> numbers)
 {
-    internal class MaxIncreasingSubArrayAsJson
+    if (numbers == null || numbers.Count == 0)
+        return JsonSerializer.Serialize(new List<int>());
+
+    List<int> subArray = new List<int>(numbers.Count);
+    List<int> maxArray = new List<int>(numbers.Count);
+    int result = 0;
+    int maxResult = 0;
+    subArray.Add(numbers[0]);
+
+    for (int i = 1; i < numbers.Count; i++)
     {
-        public static string MaxIncreasingSubarrayAsJson(List<int> numbers)
+        if (numbers[i] > numbers[i - 1])
         {
-            if (numbers == null || numbers.Count == 0)
-                return JsonSerializer.Serialize(new List<int>());
-
-            List<int> subArray = new List<int>(numbers.Count);
-            List<int> maxArray = new List<int>(numbers.Count);
-            int result = 0;
-            int maxResult = 0;
-            subArray.Add(numbers[0]);
-
-            for (int i = 1; i < numbers.Count; i++)
-            {
-                if (numbers[i] > numbers[i - 1])
-                {
-                    subArray.Add(numbers[i]);
-                    result += numbers[i];
-                }
-
-                else
-                {
-                    if (maxResult < result)
-                    {
-                        maxResult = result;
-                        maxArray = new List<int>(subArray);
-                    }
-                    subArray.Clear();
-                    subArray.Add(numbers[i]);
-                    result = numbers[i];
-                }
-
-                if (maxResult < result)
-                    maxArray = new List<int>(subArray);
-            }
-
-            return JsonSerializer.Serialize(maxArray);
+            subArray.Add(numbers[i]);
+            result += numbers[i];
         }
+
+        else
+        {
+            if (maxResult < result)
+            {
+                maxResult = result;
+                maxArray = new List<int>(subArray);
+            }
+            subArray.Clear();
+            subArray.Add(numbers[i]);
+            result = numbers[i];
+        }
+
+        if (maxResult < result)
+            maxArray = new List<int>(subArray);
     }
+
+    return JsonSerializer.Serialize(maxArray);
 }
