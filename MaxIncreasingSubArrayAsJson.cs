3426a0a18223c7d1ae9e93985eb68c47c5e9e669
Kadir Aydoğdu <121028208+aydogdu25@users.noreply.github.com>
2025-09-02 19:02:51 +0300

    feat: max sub array

---
+++ MaxIncreasingSubArrayAsJson.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace ConsoleApp2
+{
+    internal class MaxIncreasingSubArrayAsJson
+    {
+        public static string MaxIncreasingSubarrayAsJson(List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+                return JsonSerializer.Serialize(new List<int>());
+
+            List<int> subArray = new List<int>(numbers.Count);
+            List<int> maxArray = new List<int>(numbers.Count);
+            int result = 0;
+            int maxResult = 0;
+            subArray.Add(numbers[0]);
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > numbers[i - 1])
+                {
+                    subArray.Add(numbers[i]);
+                    result += numbers[i];
+                }
+
+                else
+                {
+                    if (maxResult < result)
+                    {
+                        maxResult = result;
+                        maxArray = new List<int>(subArray);
+                    }
+                    subArray.Clear();
+                    subArray.Add(numbers[i]);
+                    result = numbers[i];
+                }
+
+                if (maxResult < result)
+                    maxArray = new List<int>(subArray);
+            }
+
+            return JsonSerializer.Serialize(maxArray);
+        }
+    }
+}
